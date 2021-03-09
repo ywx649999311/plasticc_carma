2271265a33476d5435ad931f3221018aa9c47ff1
Weixiang Yu <11515291+ywx649999311@users.noreply.github.com>
2021-03-09 10:32:56 -0500

    add download lc bash

---
+++ download_test_lc.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+curl -O https://zenodo.org/record/2539456/files/plasticc_test_lightcurves_01.csv.gz
+curl -O https://zenodo.org/record/2539456/files/plasticc_test_lightcurves_02.csv.gz
+curl -O https://zenodo.org/record/2539456/files/plasticc_test_lightcurves_03.csv.gz
+curl -O https://zenodo.org/record/2539456/files/plasticc_test_lightcurves_04.csv.gz
+curl -O https://zenodo.org/record/2539456/files/plasticc_test_lightcurves_05.csv.gz
+curl -O https://zenodo.org/record/2539456/files/plasticc_test_lightcurves_06.csv.gz
+curl -O https://zenodo.org/record/2539456/files/plasticc_test_lightcurves_07.csv.gz
+curl -O https://zenodo.org/record/2539456/files/plasticc_test_lightcurves_08.csv.gz
+curl -O https://zenodo.org/record/2539456/files/plasticc_test_lightcurves_09.csv.gz
+curl -O https://zenodo.org/record/2539456/files/plasticc_test_lightcurves_10.csv.gz
+curl -O https://zenodo.org/record/2539456/files/plasticc_test_lightcurves_11.csv.gz
+
